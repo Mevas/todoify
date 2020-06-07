c899f9d569200de7a2d4baae60484c5598545012
Alexandru Vasilescu <alexandru.vasilescu01@gmail.com>
2020-06-08 01:30:41 +0300

    feat: add redis

--- config.ts
+++ config.ts
@@ -2,3 +2,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const JWT_SECRET = process.env.JWT_SECRET;
+
+export const REDIS_HOST = process.env.REDIS_HOST;
+export const REDIS_PORT = parseInt(process.env.REDIS_PORT);
+export const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+
+export const REDIS_LOGIN_ATTEMPTS_DB = parseInt(process.env.REDIS_LOGIN_ATTEMPTS_DB);
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,25 @@ import { TasksModule } from './tasks/tasks.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { RedisModule } from 'nestjs-redis';
+import { REDIS_HOST, REDIS_LOGIN_ATTEMPTS_DB, REDIS_PASSWORD, REDIS_PORT } from '../config';
 
 @Module({
-  imports: [AuthModule, UsersModule, TasksModule, PrismaModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    TasksModule,
+    PrismaModule,
+    RedisModule.register([
+      {
+        name: 'loginAttempts',
+        host: REDIS_HOST,
+        port: REDIS_PORT,
+        db: REDIS_LOGIN_ATTEMPTS_DB,
+        password: REDIS_PASSWORD,
+      },
+    ]),
+  ],
   controllers: [],
   providers: [],
 })
